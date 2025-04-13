77ef11ecd89c8c8cc8702dcc02808f5bf7cfe82a
raghul <raghulselva1114@gmail.com>
2025-04-13 20:35:52 +0530

    adding code

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './', // 👈 This line is crucial for correct file paths in deployment
   plugins: [react()],
   resolve: {
     alias: {
